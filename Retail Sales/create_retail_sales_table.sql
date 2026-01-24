5c4711a73864761ed9c664f19316b28b07571083
Nacho Rubio <rongenrola111@gmail.com>
2026-01-24 11:43:30 +0100

    Delete Retail Sales directory

--- Retail Sales/create_retail_sales_table.sql
+++
@@ -1,7 +0,0 @@
--- Forzamos a DuckDB a usar su motor
-DROP TABLE IF EXISTS retail_sales;
-
-CREATE TABLE retail_sales AS 
-SELECT * FROM read_csv_auto('C:/Users/ronge/Desktop/PROGRAMACION/DATA SCIENCE/Proyectos SQL/Analísis Retail Sales/us_retail_sales.csv');
-
-SELECT * FROM retail_sales LIMIT 10;
